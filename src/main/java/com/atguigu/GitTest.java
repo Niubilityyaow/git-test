7c8d05a1d5d9dc69a264a3030bc63133dff75f9f
Yaowa <yaowa19980102@163.com>
2021-08-30 16:13:43 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author : yaow
+ * @date : 2021-08-30 15:45
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
